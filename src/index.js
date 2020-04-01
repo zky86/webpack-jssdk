83147584443aa7cb92fcd890c7245722864f5b61
zky <272495222@qq.com>
2020-04-01 14:53:09 +0800

    添加Handlebars模板渲染

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import './style/main.scss'
 import * as utils from './lib/util'
 
+const messageList = [
+  {
+    title: '1111'
+  },
+  {
+    title: '2222'
+  },
+  {
+    title: '333'
+  }
+]
+utils.html(messageList) // 渲染模板
+
 function test () {
-  console.log(999)
+  console.log(111)
 }
 test()
--- src/lib/util.js
+++ src/lib/util.js
@@ -1,3 +1,23 @@
 export const test = (str) => {
   return str
 }
+
+// Handlebars模板使用
+export const html = (messageList) => {
+  const Handlebars = require('handlebars')
+  const html =
+  '<div class="sdk-model">' +
+    '<ul class="list clearfix"><li>这里是新闻消息第一条</li>' +
+      '{{#list}}<li>这里是新闻消息第一条{{title}}</li>{{/list}}' +
+    '</ul>' +
+  '</div>'
+  const template = Handlebars.compile(html)
+  const data = {
+    list: messageList
+  }
+  const result = template(data)
+  // 添加到body
+  const ele = document.createElement('div')
+  ele.innerHTML = result
+  document.body.appendChild(ele)
+}
