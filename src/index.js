3db9b56029815d71bb6c82cd41eda7340b2be117
zky <272495222@qq.com>
2020-04-08 12:56:23 +0800

    修复封装函数

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style/main.scss'
 import * as utils from './lib/util'
 
+const URL = '//192.168.1.253:7001' // 接口地址
+
 const messageList = [
   {
     title: '1111'
@@ -12,9 +14,11 @@ const messageList = [
     title: '333'
   }
 ]
-utils.html(messageList) // 渲染模板
 
-function test () {
-  console.log(111)
-}
-test()
+// function test () {
+//   console.log(111)
+// }
+// test()
+utils.fetch(`${URL}/article`, function (res) {
+  utils.html(res) // 渲染模板
+})
--- src/lib/util.js
+++ src/lib/util.js
@@ -2,18 +2,37 @@ export const test = (str) => {
   return str
 }
 
+/**
+ * 请求
+ */
+export const fetch = function (url, callback, data, x) {
+  try {
+    x = new (window.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0')
+    x.open(data ? 'POST' : 'GET', url, 1)
+    // x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    x.withCredentials = true
+    // x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    x.onreadystatechange = function () {
+      x.readyState > 3 && callback && callback(JSON.parse(x.responseText), x) // 返回json
+    }
+    x.send(data)
+  } catch (e) {
+    window.console && console.log(e)
+  }
+}
+
 // Handlebars模板使用
-export const html = (messageList) => {
+export const html = (para) => {
   const Handlebars = require('handlebars')
   const html =
   '<div class="sdk-model">' +
-    '<ul class="list clearfix"><li>这里是新闻消息第一条</li>' +
-      '{{#list}}<li>这里是新闻消息第一条{{title}}</li>{{/list}}' +
+    '<ul class="list clearfix">' +
+      '{{#list}}<li>{{title}}</li>{{/list}}' +
     '</ul>' +
   '</div>'
   const template = Handlebars.compile(html)
   const data = {
-    list: messageList
+    list: para
   }
   const result = template(data)
   // 添加到body
